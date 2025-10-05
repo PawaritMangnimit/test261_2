b0d0701900ac0e83bedba90a779ed4c4142f928a
PawaritMangnimit <pawarit.man@dome.tu.ac.th>
2025-10-05 19:27:09 +0700

    Update PingController.java

--- backend/src/main/java/com/unijobs/campus/PingController.java
+++ backend/src/main/java/com/unijobs/campus/PingController.java
@@ -1,1 +1,13 @@
+package com.unijobs.campus;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PingController {
+  @GetMapping("/ping")
+  public String ping() {
+    return "pong";
+  }
+}
 
