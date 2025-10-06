bf00c025be42c06c35b368f0b9a36ce2f2c81799
PawaritMangnimit <pawarit.man@dome.tu.ac.th>
2025-10-06 12:53:16 +0700

    Update CampusApplication.java

--- backend/src/main/java/com/unijobs/campus/CampusApplication.java
+++ backend/src/main/java/com/unijobs/campus/CampusApplication.java
@@ -1,12 +1,8 @@
 package com.unijobs.campus;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CampusApplication {
-  public static void main(String[] args) {
-    SpringApplication.run(CampusApplication.class, args);
-  }
+  public static void main(String[] args) { SpringApplication.run(CampusApplication.class, args); }
 }
-
